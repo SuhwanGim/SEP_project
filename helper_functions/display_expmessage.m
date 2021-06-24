025ed15fac89f3d22a6562603007c21255f5b36c
Suhwan Gim <suhwan.gim.psych@gmail.com>
2021-06-24 20:37:35 +0900

    edit disaply_expmessage (color)

--- helper_functions/display_expmessage.m
+++ helper_functions/display_expmessage.m
@@ -8,8 +8,8 @@ global theWindow white bgcolor window_rect fontsize; % rating scale
 EXP_start_text = double(msg);
 
 % display
-Screen(theWindow,'FillRect',bgcolor, window_rect);
-DrawFormattedText2([double(sprintf('<size=%d><font=-:lang=ko><color=ff0000>',fontsize)) EXP_start_text],'win',theWindow,'sx','center','sy','center','xalign','center','yalign','center');
+%Screen(theWindow,'FillRect',bgcolor, window_rect);
+DrawFormattedText2([double(sprintf('<size=%d><font=-:lang=ko><color=ffffff>',fontsize)) EXP_start_text],'win',theWindow,'sx','center','sy','center','xalign','center','yalign','center');
 %DrawFormattedText(theWindow, EXP_start_text, 'center', 'center', white, [], [], [], 1.5);
 Screen('Flip', theWindow);
 
